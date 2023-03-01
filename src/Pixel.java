dd6f34ace661ea31af059e4187b845f404a53657
Ben Clott <bclott44@gmail.com>
2023-03-01 15:52:09 -0500

    Finished intensity, used cast?? 3/1 1551

--- src/Pixel.java
+++ src/Pixel.java
@@ -14,6 +14,7 @@ public class Pixel {
     this.rgba = new int[4];
     this.coords = coords;
     value = Arrays.stream(rgba).max().getAsInt();
+    intensity = (int)Math.ceil(Arrays.stream(rgba).average().orElse(Double.NaN));
 
   }
 }
